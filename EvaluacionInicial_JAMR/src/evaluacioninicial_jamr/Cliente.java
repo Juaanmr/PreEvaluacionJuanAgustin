e21803bb75ec2c82fc830aec6ee5fea73bcf0bf3
Juaanmr <juanagus0718@gmail.com>
2023-12-05 14:24:51 +0100

    Añadiendo comentarios

--- EvaluacionInicial_JAMR/src/evaluacioninicial_jamr/Cliente.java
+++ EvaluacionInicial_JAMR/src/evaluacioninicial_jamr/Cliente.java
@@ -13,13 +13,17 @@ import java.util.Scanner;
  *
  * @author Juan
  */
-public class Cliente {
 
+public class Cliente {
+    //creacion de variables que usaremos en los siguetnetes metodos
     private int id_cliente;
     private String nombre;
     private String direccion;
     private String telefono;
 
+    /**
+     * metodo donde pido las datos al usuario y que se almacenaran en las variables nateriores
+     */
     public void pedirDatos() {
         Scanner sc = new Scanner(System.in);
 
@@ -38,11 +42,16 @@ public class Cliente {
         sc.nextLine();
     }
 
+    /**
+     * metodo donde se guardaran los datos de los clientes en un fichero
+     */
     public void guardarDatos() {
+        //nombre del fichero y ubicacion de donde se va a crear 
         String fichero = "archivos/misClientes.txt";
         
         File archivo = new File(fichero);
         
+        //si el fichero ya existe sale este mensaje y se añade al nuevo cliente
         if(archivo.exists())
             System.out.println("\nEl fichero ya existe en esa ubicacion\n");
         try(BufferedWriter escritura = new BufferedWriter(new FileWriter(fichero))){
@@ -52,6 +61,10 @@ public class Cliente {
         } 
     }
 
+    /**
+     * metodo que muestra por pantalla y en el fichero los datos del cliente nuevo
+     * @return devuelve los daatos del cliente
+     */
     @Override
     public String toString() {
         return "Cliente{" + "id_cliente=" + id_cliente + ", nombre=" + nombre + ", direccion=" + direccion + ", telefono=" + telefono + '}';
